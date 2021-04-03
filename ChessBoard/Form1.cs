a412d4cc3551feb2fbc2ea9a30ba3d97b88f9232
Aygün Nəcəfova <aygun.nacafova.577@teams.dia.edu.az>
2021-04-03 15:05:10 +0400

    Add project files.

---
+++ ChessBoard/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8];
+            int top = 0, left = 0;
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Top = top;
+                    buttons[i, j].Left = left;
+                    left += 50;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    this.Controls.Add(buttons[i, j]);
+                }
+
+                left = 0;
+                top += 50;
+            }
+        }
+    }
+}
